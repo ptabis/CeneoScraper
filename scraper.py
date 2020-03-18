5471a3683a69b241d8c15c296b6d71c528626ef3
ptabis <p.tabis@protonmail.com>
2020-03-18 14:46:28 +0100

    PT 2020-03-18

--- scraper.py
+++ scraper.py
@@ -12,16 +12,40 @@ page_tree = BeautifulSoup(page_response.text, 'html.parser')
 opinions = page_tree.find_all("li", "review-box")
 
 #wydobycie składowych dla pojedynczej opinii
-opinion = opinions.pop()
+for opinion in opinions:
+    opinion_id = opinion["data-entry-id"]
+    author = opinion.find("div", "reviewer-name-line").string
+    try:
+        recomendation = opinion.find("div", "product-review-summary").find("em").string
+    except AttributeError:
+        recomendation = None
 
+    stars = opinion.find("span", "review-score-count")
+    try:
+        purchased = opinion.find("div", "product-review-pz")
+    except AttributeError:
+        purchased = None
 
-opinion_id = opinion["data-entry-id"]
-author = opinion.find("div", "reviewer-name-line").string
-recomendation = opinion.find("div", "product-review-summary").find("em").string
-stars = opinion.find("span", "review-score-count")
-purchased = opinion.find("div", "product-review-pz")
-useful = opinion.find("button", "vote-yes").find("span").string
-useless = opinion.find("button", "vote-no").find("span").string
-content = opinion.find("p", "product-review-body").get_text()
+    dates = opinion.find("span", "review-time").find_all("time")
+    review_date = dates.pop(0)["datetime"]
+    try:
+        purchase_date = dates.pop(0)["datetime"]
+    except AttributeError:
+        purchase_date = None
 
-print(opinion_id, author, recomendation, useful, useless, content, stars, purchased)
+    useful = opinion.find("button", "vote-yes").find("span").string
+    useless = opinion.find("button", "vote-no").find("span").string
+    content = opinion.find("p", "product-review-body").get_text()
+
+    try:
+        pros = opinion.find("div", "pros-cell").find("ul").get_text()
+    except AttributeError:
+        pros = None
+
+    try:
+        cons = opinion.find("div", "cons-cell").find("ul").get_text()
+    except AttributeError:
+        cons = None
+
+    #print(opinion_id, author, recomendation, useful, useless, content, stars, purchased)
+    print(opinion_id, recomendation, stars, content, author, pros, cons, useful, useless, purchased, purchase_date, review_date)
